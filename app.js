51053b0461d18dd31f0f3d6dd0c635d5a60f5856
Heriberto Prieto <HPrieto@ITs-MacBook-Pro.attlocal.net>
2018-09-18 18:29:23 -0700

    trying to unfuck things

--- app.js
+++ app.js
@@ -15,4 +15,6 @@ app.set('view engine', 'hjs');
 app.use('/', indexRouter);
 
 // Begin server
-app.listen(3000, () => console.log('Listening to Port: 3000'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
