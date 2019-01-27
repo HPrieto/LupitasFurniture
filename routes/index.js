de8646d53b1d2939da170e969c3453af4bd4ea02
Heriberto Prieto <HPrieto@ITs-MacBook-Pro.attlocal.net>
2019-01-27 00:40:31 -0800

    JavaScript rating section somewhat done... not really

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,18 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var masterEmail = 'lupitasfurniturewebsite@gmail.com';
+var firebase = require('firebase');
+var path = require('path');
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAcVkAlPudoOqXwchD_aOu2mhXV_H-mRqY",
+    authDomain: "lupitasfurniture-f583c.firebaseapp.com",
+    databaseURL: "https://lupitasfurniture-f583c.firebaseio.com",
+    projectId: "lupitasfurniture-f583c",
+    storageBucket: "lupitasfurniture-f583c.appspot.com",
+    messagingSenderId: "75776435304"
+};
+firebase.initializeApp(firebaseConfig);
 
 var transporter = nodemailer.createTransport({
 	service: 'gmail',
@@ -13,7 +25,38 @@ var transporter = nodemailer.createTransport({
 
 router.get('/', function (req, res, next) {
 	res.render('index', { title: 'Express' });
-	// res.render('rating_modal');
+});
+
+router.get('/reviews', function (req, res, next) {
+	res.render('reviews_modal');
+});
+
+router.post('/ratingsubmit', function (req, res, next) {
+	if (typeof req === 'undefined') {
+		return
+	}
+
+	var currentTime = new Date();
+	var name = req.query.name;
+	var rating = req.query.rating;
+	var comment = req.query.comment;
+	var ip = req.connection.remoteAddress;
+
+	if (isNaN(rating)) {
+		return
+	}
+
+	return;
+	var database = firebase.database();
+	var ref = database.ref('ratings');
+
+	ref.set({
+		name: name,
+		rating: rating,
+		comment: comment,
+		ip: ip
+	});
+
 });
 
 router.get('/emailrequest', function (req, res, next) {
